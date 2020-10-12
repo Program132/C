e13e352f757666c8f41cf188b4b269e5b6551998
beyptitloup <67320703+beyptitloup@users.noreply.github.com>
2020-10-12 20:16:12 +0200

    Math Modules

---
+++ cpp/modules/Modules/Modules/math.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int remove(int a, int b)
+{
+	return a - b;
+}
+
+int divi(int a, int b)
+{
+	return a / b;
+}
+
+int multi(int a, int b)
+{
+	return a * b;
+}
+
+int doubleNbr(int nbr)
+{
+	return nbr * nbr;
+}
+
+int tripleNbr(int nbr)
+{
+	return nbr * nbr * nbr;
+}
