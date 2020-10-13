7d8e019e7d4e8cb2d38454f6b23d789eb7504051
beyptitloup <67320703+beyptitloup@users.noreply.github.com>
2020-10-13 07:14:00 +0200

    Module : code

---
+++ cpp/modules/Modules 2020/Modules/code.cpp
@@ -0,0 +1,24 @@
+#include "code.h"
+#include <iostream>
+
+using namespace std;
+
+void info()
+{
+	cout << "The modules has been create for developer beginner" << endl;
+}
+
+void math()
+{
+	cout << "The module math has : add, remove, multi, divi, doubleNbr, tripleNbr." << endl;
+}
+
+void code()
+{
+	cout << "The module code has : math, code, info, learn_cpp." << endl;
+}
+
+void learn_cpp()
+{
+	cout << "The langage C++ is is object oriented, and he is not easy at learn. I can advise openclassrooms and Youtube." << endl;
+}
---
+++ cpp/modules/Modules 2020/Modules/code.h
@@ -0,0 +1,9 @@
+#ifndef CODE_H
+#define CODE_H
+
+void info();
+void math();
+void code();
+void learn_cpp();
+
+#endif
